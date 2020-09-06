fa7b447127f4896d8bdacd5da8e4d410ea1e760a
Nijat Muhtar <yasunaz1990@gmail.comyasunaz1990@gmail.comyasunaz1990@gmail.com>
2020-09-06 16:29:21 -0400

    i made some edit on NijatDemo

--- src/NijatDemo.java
+++ src/NijatDemo.java
@@ -2,5 +2,9 @@ public class NijatDemo {
 
     public static void main(String[] args) {
 
+        System.out.println("hello guys!");
+        for(int i = 0; i < 10; i++) {
+            System.out.println("hello there guys!");
+        }
     }
 }
