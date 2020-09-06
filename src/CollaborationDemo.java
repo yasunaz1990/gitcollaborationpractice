bcbe7d98e3176c1ad0a9c1decca3ada5ed61b907
Nijat Muhtar <yasunaz1990@gmail.comyasunaz1990@gmail.comyasunaz1990@gmail.com>
2020-09-06 16:55:34 -0400

    i have coded on CollaborationDemo.java

--- src/CollaborationDemo.java
+++ src/CollaborationDemo.java
@@ -3,8 +3,13 @@ public class CollaborationDemo {
     public static void main(String[] args) {
 
         // section A
+        System.out.println("Nijat is the lead tech guy");
+        String title = "Sr. Developer";
+        boolean isCool = true;
 
         // section B
+        int number = 10;
+        boolean summer = true;
 
         // section C
 
